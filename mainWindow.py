d38c537167e6f79ccf35459e49c10e09c3d44049
Kristin <1358365@DESKTOP-4FH9G41>
2019-06-10 13:03:54 +0500

    files loading added

--- mainWindow.py
+++ mainWindow.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtWidgets import QAction, QFileDialog
 from PyQt5.QtGui import QImage, QPixmap
 from App import Ui_MainWindow
 import pydicom
@@ -12,5 +13,55 @@ class mainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.file1.setText("Path to file 1:")
-        self.ui.file2.setText("Path to file 2:")
+        self.setWindowTitle('Dicom Viewer')
+
+        self.ui.file1searchButton.clicked.connect(self.showFile1SearchDialog)
+        self.ui.file2searchButton.clicked.connect(self.showFile2SearchDialog)
+
+        self.ui.loadFilesButton.clicked.connect(self.loadFiles)
+
+    def showFile1SearchDialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '')[0]
+        self.ui.file1Path.setText(fname)
+
+    def showFile2SearchDialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '')[0]
+        self.ui.file2path.setText(fname)
+
+    def loadFiles(self):
+        firstFileName = self.ui.file1Path.text()
+        secondFileName = self.ui.file2path.text()
+
+        print(parseFileName(firstFileName))
+        print(parseFileName(secondFileName))
+
+        dicomFile1 = pydicom.dcmread(firstFileName)
+        dicomFile2 = pydicom.dcmread(secondFileName)
+
+        image1 = dicomFile1.pixel_array
+        image2 = dicomFile2.pixel_array
+
+        qimage1 = QImage(image1.data, image1.shape[1], image1.shape[0] , QImage.Format_RGB444)
+        self.ui.image1.resize(image1.shape[1], image1.shape[0])
+
+        qimage2 = QImage(image2.data, image2.shape[1], image2.shape[0], QImage.Format_RGB444)
+        self.ui.image2.resize(image2.shape[1], image2.shape[0])
+
+        pixmap1 = QPixmap(qimage1)
+        pixmap2 = QPixmap(qimage2)
+
+        self.ui.image1.setPixmap(pixmap1)
+        self.ui.image2.setPixmap(pixmap2)
+
+        resultedImage = functions.imposeImages(image1, image2)
+
+        qresultedImage = QImage(resultedImage.data, resultedImage.shape[1], resultedImage.shape[0], QImage.Format_RGB444)
+        self.ui.resultedImage.resize(resultedImage.shape[1], resultedImage.shape[0])
+
+        pixmapResultedImage = QPixmap(qresultedImage)
+
+        self.ui.resultedImage.setPixmap(pixmapResultedImage)
+
+
+def parseFileName(fileName):
+    return fileName.replace('/', '\\')
